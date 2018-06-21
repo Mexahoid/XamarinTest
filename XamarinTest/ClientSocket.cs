043b38da094fcc24ef4691c3191a06e782ffe49d
Mexahoid <mirramax97@Mail.ru>
2018-06-21 14:19:38 +0300

    Добавьте файлы проекта.

---
+++ XamarinTest/ClientSocket.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XamarinTest
+{
+    class ClientSocket
+    {
+        private string addr;
+        private int port;
+        private TcpClient tcpClient;
+        private NetworkStream networkStream;
+
+        private static ClientSocket instance;
+
+        private ClientSocket(string addr, int port)
+        {
+            this.addr = addr;
+            this.port = port;
+        }
+
+        public static ClientSocket GetInstance(string addr, int port)
+        {
+            return instance ?? (instance = new ClientSocket(addr, port));
+        }
+        
+        
+
+        public async Task Connect()
+        {
+            tcpClient = new TcpClient();
+            //Console.WriteLine("[Client] Connecting to server");
+            await tcpClient.ConnectAsync(addr, port);
+            //Console.WriteLine("[Client] Connected to server");
+            networkStream = tcpClient.GetStream();
+        }
+
+        public async Task<string> ClientWork(string text)
+        {
+            string res = await SendMessage(text);
+            return $"Server response was {res}";
+            //Console.WriteLine("[Client] Server response was {0}", res);
+            //}
+        }
+
+        private async Task<string> SendMessage(string message)
+        {
+            //Console.WriteLine($"[Client] Writing request {message}");
+            var bytes = Encoding.UTF8.GetBytes(message);
+            await networkStream.WriteAsync(bytes, 0, bytes.Length);
+
+            var buffer = new byte[4096];
+            var byteCount = await networkStream.ReadAsync(buffer, 0, buffer.Length);
+            //networkStream.Flush();
+            return Encoding.UTF8.GetString(buffer, 0, byteCount);
+        }
+
+        public void Disconnect()
+        {
+            if (networkStream != null)
+            {
+                networkStream.Flush();
+                networkStream.Close();
+            }
+
+            if (tcpClient != null && tcpClient.Connected)
+                tcpClient.Close();
+        }
+    }
+}
---
+++ XamarinTest/MainActivity.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+
+namespace XamarinTest
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button btn_connect, btn_send;
+        private EditText textbox_server, textbox_send, textbox_receive;
+        private ClientSocket clsock;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            clsock = ClientSocket.GetInstance("192.168.1.35", 1488);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+
+            btn_connect = FindViewById<Button>(Resource.Id.btn_connect);
+            btn_send = FindViewById<Button>(Resource.Id.btn_send);
+
+            textbox_server = FindViewById<EditText>(Resource.Id.edittext_server_ip);
+            textbox_send = FindViewById<EditText>(Resource.Id.edittext_message);
+            textbox_receive = FindViewById<EditText>(Resource.Id.edittext_response);
+            btn_connect.Click += Connect;
+            btn_send.Click += Send;
+
+
+            if (savedInstanceState == null || !savedInstanceState.ContainsKey("server") ||
+                !savedInstanceState.ContainsKey("message") || !savedInstanceState.ContainsKey("reply"))
+                return;
+            textbox_send.Text = savedInstanceState.GetString("message");
+            textbox_server.Text = savedInstanceState.GetString("server");
+            textbox_receive.Text = savedInstanceState.GetString("reply");
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            outState.PutString("message", textbox_send.Text);
+            outState.PutString("server", textbox_server.Text);
+            outState.PutString("reply", textbox_receive.Text);
+
+            base.OnSaveInstanceState(outState);
+        }
+        
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            clsock.ClientWork("Disconnect").GetAwaiter().GetResult();
+            clsock.Disconnect();
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private async void Connect(object sender, EventArgs eventArgs)
+        {
+            await clsock.Connect();
+            textbox_server.Text = "IP сервера: 192.168.1.35";
+        }
+
+        private async void Send(object sender, EventArgs e)
+        {
+            textbox_receive.Text = await clsock.ClientWork(textbox_send.Text);
+            textbox_send.Text = "";
+        }
+
+    }
+}
+
